d90df329fc03c7d230fa3456b574c84bbe2a9b58
Sipho <sip.zuma.com>
2025-08-10 07:18:01 +0200

    ui: export WeeklyMissionListView

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,6 +1,7 @@
 export * from './tokens';
 export { WeekHeaderView } from './week/WeekHeader.view';
 export { WeeklyMissionItemView } from './week/WeeklyMissionItem.view';
+export { WeeklyMissionListView } from './week/WeeklyMissionList.view';
 export { ModuleCardView } from './modules/ModuleCard.view';
 export { ModuleQuickView } from './modules/ModuleQuickView.view';
 export { SemesterSnapshotView } from './semester/SemesterSnapshot.view';
